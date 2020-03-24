e37221e35130d4904d9a532bbdb36685f05d54aa
DrInnovatio <turing1723@gmail.com>
2020-03-24 16:37:55 +1000

    added firebase into application, created sign in pagewith sign in component and google sign in

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
-const Custombutton = ({children, ...otherProps}) => (
-  <button className='custom-button' {...otherProps}>
+const Custombutton = ({children, isGoogleSignIn, ...otherProps}) => (
+  <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button `} 
+  {...otherProps}>
     {children}
   </button>
 )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -24,7 +24,6 @@ class SignIn extends React.Component{
     this.setState({[name]: value})
   }
 
-
   render(){
     return (
       <div className='sign-in'>
@@ -32,18 +31,21 @@ class SignIn extends React.Component{
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
+
           <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required/>
-
-
 
           <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required/>
 
+          <div className='buttons'>
 
-          <CustomButton type='submit' >Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            {' '}
-             Sign in with Google{' '}
-          </CustomButton>
+            <CustomButton type='submit' >Sign In</CustomButton>
+            
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              {' '} Sign in with Google {' '}
+            </CustomButton>
+
+          </div>
+
 
         </form>
       </div>
