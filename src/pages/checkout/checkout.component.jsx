6c2f86566852798b13782113bfe1d629234ba879
DrInnovatio <turing1723@gmail.com>
2020-05-05 18:53:28 +1000

    installed Stripe and payment button

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import { createStructuredSelector } from 'reselect'
 
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors'
 
@@ -40,6 +41,12 @@ const CheckoutPage = ({cartItems, total}) => (
       ))}
 
     <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+        *Please use the following test credit card for payments* 
+        <br/>
+        4242 4242 4242 4242 - Exp: 01/20 - cvv: 123
+    </div>
+    <StripeCheckoutButton price={total}/>
   </div>
 );
 
@@ -48,4 +55,4 @@ const mapStateToProps = createStructuredSelector({
   total: selectCartTotal
 });
 
-export default connect(mapStateToProps)(CheckoutPage);
+export default connect(mapStateToProps)(CheckoutPage); 
